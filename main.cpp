415ac694a3360ae7a9bfa627ba000f53a74a7f5a
Anderson Evangelista <andersonevann@outlook.com>
2018-05-21 15:58:15 -0300

    alteração no destrutor de do .h e alteração na main(falta converter maisculas para minusculas)

--- main.cpp
+++ main.cpp
@@ -5,19 +5,49 @@
 
 using namespace std;
 
-//using std::string;
-//using std::cout;
-//using std::endl;
-
 int main(int argc, char const *argv[]) {
-	//bool teste;
+	bool teste = true;
 
 	ListaLigada<string> lista;
-	lista.InsereNoFinal("a");
+	lista.InsereNoFinal("s");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal("c");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal("r");
 	lista.InsereNoFinal("r");
 	lista.InsereNoFinal("a");
-	lista.InsereNoFinal("r");
+	lista.InsereNoFinal("m");
+	lista.InsereNoFinal("-");
+	lista.InsereNoFinal("m");
+	lista.InsereNoFinal("e");
+	lista.InsereNoFinal(",");
+	lista.InsereNoFinal(" ");
+	lista.InsereNoFinal("s");
+	lista.InsereNoFinal("u");
+	lista.InsereNoFinal("b");
+	lista.InsereNoFinal("i");
+	lista.InsereNoFinal(" ");
+	lista.InsereNoFinal("n");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal(" ");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal("n");
+	lista.InsereNoFinal("i");
+	lista.InsereNoFinal("b");
+	lista.InsereNoFinal("u");
+	lista.InsereNoFinal("s");
+	lista.InsereNoFinal(" ");
+	lista.InsereNoFinal("e");
+	lista.InsereNoFinal("m");
+	lista.InsereNoFinal(" ");
+	lista.InsereNoFinal("m");
 	lista.InsereNoFinal("a");
+	lista.InsereNoFinal("r");
+	lista.InsereNoFinal("r");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal("c");
+	lista.InsereNoFinal("o");
+	lista.InsereNoFinal("s");
 
 	shared_ptr<Node<string>> aux = lista.getCabeca();
 
@@ -28,7 +58,33 @@ int main(int argc, char const *argv[]) {
 		aux = aux->getNext();
 	}
 
-	string num = pilha.pop();
+	aux = lista.getCabeca();
+
+	while(aux != nullptr) {
+		if(aux->getValor() == "," || aux->getValor() == " " || aux->getValor() == "-") {
+			aux = aux->getNext();
+			continue;
+		}
+
+		if(pilha.top() == "," || pilha.top() == " " || pilha.top() == "-") {
+			pilha.pop();
+			continue;
+		}
+
+		if(aux->getValor() != pilha.top()) {
+			teste = false;
+			aux = nullptr;
+		} else {
+			aux = aux->getNext();
+			pilha.pop();
+		}
+	}
+
+	if(teste) {
+		cout << "E palindromo!" << endl;
+	} else {
+		cout << "Nao e palindromo." << endl;
+	}
 
 	return 0;
 }
